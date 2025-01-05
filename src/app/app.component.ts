f31ab519ffe2e8533b561672a42129bac35e00c9
Lumonbel <lmonfortbeltran@gmail.com>
2025-01-05 12:36:01 +0100

    footer y formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [FormularioComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
