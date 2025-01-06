450826adb1d938c8623fb09f6c98c227de2ac4dc
Lumonbel <lmonfortbeltran@gmail.com>
2025-01-06 20:46:36 +0100

    ver prenda

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormularioComponent } from './components/formulario/formulario.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { VerprendaComponent } from "./components/verprenda/verprenda.component";
 
 @Component({
   selector: 'app-root',
-  imports: [FormularioComponent,FooterComponent],
+  imports: [FormularioComponent, FooterComponent, VerprendaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
