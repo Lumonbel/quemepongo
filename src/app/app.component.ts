602de481ba8fe1085606f6914908c99709fd2912
GerardNavarro33 <gerardnavarro36@gmail.com>
2025-01-17 13:57:07 +0100

    inicio de hacer el login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,11 @@ import { VerprendaComponent } from "./components/verprenda/verprenda.component";
 import { ComponentepruebaComponent } from "./components/componenteprueba/componenteprueba.component";
 import { UsuarioComponent } from './components/usuario/usuario.component';
 import { IndexComponent } from './components/index/index.component';
+import { LoginComponent } from './components/login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [FormularioComponent, FooterComponent, VerprendaComponent, ComponentepruebaComponent, UsuarioComponent, IndexComponent],
+  imports: [FormularioComponent, FooterComponent, VerprendaComponent, ComponentepruebaComponent, UsuarioComponent, IndexComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
