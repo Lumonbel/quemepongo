c271d9719017659409891e4c9131e57ad34bd2a5
GerardNavarro <gerardnavarro36@gmail.com>
2025-01-23 22:20:12 +0100

    Chips acabados y funcionando

--- src/app/components/verprenda/verprenda.component.ts
+++ src/app/components/verprenda/verprenda.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Chip } from 'primeng/chip';
 import { Select } from 'primeng/select';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 interface Prenda {
   ropa: string;
@@ -9,16 +10,35 @@ interface Prenda {
 
 @Component({
   selector: 'app-verprenda',
-  imports: [Chip, Select, FormsModule],
+  imports: [Chip, Select, FormsModule, CommonModule],
   standalone: true,
   templateUrl: './verprenda.component.html',
   styleUrl: './verprenda.component.css'
 })
 export class VerprendaComponent {
-  prendasSel: Prenda[] | undefined;
-
+  prendasSel: Prenda[] = [];
   prendaSeleccionada: Prenda | undefined;
-  
+  mostrarDiv: string = '';
+
+  chipsRopa: string[] = ["Pantalones", "Camisas", "Chaquetas", "Sudaderas", "Faldas", "Jerseys","Vestidos","Baño"];
+  chipsZapatos: string[] = [];
+  chipsComplementos: string[] = ["Cinturones", "Corbatas", "Gorras", "Bufandas", "Guantes"];
+
+  chipsSeleccionados: string[] = [];
+
+  chipSeleccionadoFunc(chip: string) {
+    const index = this.chipsSeleccionados.indexOf(chip);
+    if (index > -1) {
+      this.chipsSeleccionados.splice(index, 1);
+    } else {
+      this.chipsSeleccionados.push(chip);
+    }
+  }
+
+  funcSeleccionado(chip: string): boolean {
+    return this.chipsSeleccionados.includes(chip);
+  }
+
   ngOnInit() {
     this.prendasSel = [
       { ropa: 'Ropa' },
@@ -27,10 +47,14 @@ export class VerprendaComponent {
     ];
   }
 
-
-
   onPrendaChange(event: any) {
-    this.prendaSeleccionada = event.value;
-    console.log(this.prendaSeleccionada);
-  }    
+    const selectedOption = this.prendasSel.find(prenda => prenda.ropa === event.value.ropa);
+    console.log('Opción seleccionada:', selectedOption);
+
+    if (selectedOption) {
+      this.mostrarDiv = selectedOption.ropa;
+    } else {
+      this.mostrarDiv = '';
+    }
+  }
 }
