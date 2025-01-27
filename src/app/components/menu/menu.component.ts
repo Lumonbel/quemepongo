5f8f2aec5fbd6415be4abc37718a18152c2c52e8
pazcasasperez <marcasper16@alu.edu.gva.es>
2025-01-27 09:44:53 +0100

    cambio nombre categoria almanenaje

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -25,7 +25,7 @@ export class MenuComponent implements OnInit{
                 icon: '../../../assets/iconos/AlmacenamientoP.svg',
                 items: [
                     {
-                    label: 'Almacenaje',
+                    label: 'Bolsos',
                     icon: '../../../assets/iconos/AlmacenamientoP.svg',
                     },
                     {
