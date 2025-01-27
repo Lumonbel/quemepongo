5d24da475ce455ab6838b89cd3e875f2b4b5aeeb
GerardNavarro33 <gerardnavarro36@gmail.com>
2025-01-27 12:05:54 +0100

    de aqui importa el font Vazir-b y el borde claro de los forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,15 @@ import { UsuarioComponent } from './components/usuario/usuario.component';
 import { IndexComponent } from './components/index/index.component';
 import { LoginComponent } from './components/login/login.component';
 import { FormularioPasosComponent } from './components/formulario-pasos/formulario-pasos.component';
-VerprendaComponent } from './components/verprenda/verprenda.component';
-
+import { VerprendaComponent } from './components/verprenda/verprenda.component';
 import { BtAtrasComponent } from './components/bt-atras/bt-atras.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { InicioClienteComponent } from './components/inicio-cliente/inicio-cliente.component';
+import { VerPerfilClienteComponent } from "./components/ver-perfil-cliente/ver-perfil-cliente.component";
 
 @Component({
   selector: 'app-root',
-  imports: [FormularioComponent, FooterComponent, VerprendaComponent, ComponentepruebaComponent, UsuarioComponent, IndexComponent, LoginComponent, FormularioPasosComponent, RouterOutlet, BtAtrasComponent, MenuComponent, InicioClienteComponent],
+  imports: [FormularioComponent, FooterComponent, VerprendaComponent, ComponentepruebaComponent, UsuarioComponent, IndexComponent, LoginComponent, FormularioPasosComponent, RouterOutlet, BtAtrasComponent, MenuComponent, InicioClienteComponent, VerPerfilClienteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   encapsulation: ViewEncapsulation.None,
