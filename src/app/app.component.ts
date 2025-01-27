3a11369087561ff22c95eb1e6c3cf2db97c1d65e
Lumonbel <lmonfortbeltran@gmail.com>
2025-01-27 23:57:30 +0100

    mejora diseño infoarticulo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { VerprendaComponent } from './components/verprenda/verprenda.component';
 import { DatosarticuloComponent } from "./components/datosarticulo/datosarticulo.component";
+import { IndexComponent } from "./components/index/index.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, FooterComponent, RouterOutlet, VerprendaComponent, DatosarticuloComponent],
+  imports: [HeaderComponent, FooterComponent, RouterOutlet, VerprendaComponent, DatosarticuloComponent, IndexComponent],
 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
