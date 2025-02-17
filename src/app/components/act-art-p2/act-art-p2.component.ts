b6d3f22829c98246577570d1358e9df71e9c4e3c
pazcasasperez <marcasper16@alu.edu.gva.es>
2025-02-17 11:58:57 +0100

    Cambios para previsualizar y actualizar articulo

--- src/app/components/act-art-p2/act-art-p2.component.ts
+++ src/app/components/act-art-p2/act-art-p2.component.ts
@@ -5,6 +5,8 @@ import { CommonModule } from '@angular/common';
 import { BtAtrasComponent } from '../bt-atras/bt-atras.component';
 import { AuthService } from '../../services/auth.service';
 import { UsuarioService } from '../../services/usuario.service';
+import { SweetAlertArrayOptions } from 'sweetalert2';
+import { AlertService } from '../../services/alerts.service';
 //import { jwtDecode } from 'jwt-decode';
 
 
@@ -19,6 +21,8 @@ import { UsuarioService } from '../../services/usuario.service';
   ]
 })
 export class ActArtP2Component implements OnInit {
+  previewImage: string  | null = null;
+  imagenSeleccionada: File | null = null;
   articulo: any = {
     id: '',
     color: '',
@@ -60,6 +64,7 @@ export class ActArtP2Component implements OnInit {
     private articuloService: ArticulosService,
     private authService: AuthService,
     private usuarioService: UsuarioService,
+    private alerta: AlertService,
   ) { }
 
   categoriaSeleccionada: string = '';
@@ -74,6 +79,13 @@ export class ActArtP2Component implements OnInit {
         console.log(this.articulo);
         // Datos recibidos de la base de datos
         this.subcategoriaSeleccionada = this.articulo.tipo;
+        if (this.articulo?.imagen) {
+          this.previewImage = 'data:image/png;base64,' + this.articulo.imagen;
+          // Asignar imagen en el objeto artículo
+          this.articulo.imagen = this.previewImage;
+        }
+
+
 
         // Encontrar y establecer la categoría correspondiente
         for (let categoria of this.categorias) {
@@ -102,6 +114,14 @@ export class ActArtP2Component implements OnInit {
   actualizarCategoria() {
     console.log('Actualizando producto:', this.articulo);
     this.articulo.tipo = this.subcategoriaSeleccionada;
+    // Eliminar el prefijo "data:image/png;base64,"
+    if(this.previewImage){
+      const base64Data = this.previewImage.split(',')[1];
+
+    // Ahora puedes enviar base64Data al backend
+    this.articulo.imagen = base64Data;
+    }
+    
     const nombreUsuario = localStorage.getItem('nombreUsuario');
     if (nombreUsuario) {
       this.usuarioService.findByNombreUsuario(nombreUsuario).subscribe({
@@ -111,7 +131,7 @@ export class ActArtP2Component implements OnInit {
           this.articuloService.updateArticulo(this.articulo).subscribe({
             next: (response) => {
               console.log('Producto actualizado:', response);
-              alert('Producto actualizado correctamente');
+              this.alerta.success("Actualización correcta", "Su actualización se ha realizado correctamente")
             },
             error: (err) => {
               console.error('Error al actualizar:', err);
@@ -124,6 +144,30 @@ export class ActArtP2Component implements OnInit {
       });
     } else {
       console.error('No se encontró el nombre de usuario en el localStorage');
+    }
+  }
+
+  previsualizarImagen(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files[0]) {
+      const file = input.files[0];
+      const reader = new FileReader();
+      this.imagenSeleccionada = file;
+      // Se ejecuta cuando el lector termina de cargar el archivo
+      reader.onload = () => {
+        //this.previewImage = reader.result;
+        this.previewImage = reader.result as string;
+        this.articulo.imagen = this.previewImage;
+      };
+
+      // Leemos el archivo como Data URL (base64)
+      reader.readAsDataURL(file);
+      /*
+      this.formulario.patchValue({ foto: file });
+      this.imagenSeleccionada = file;
+      const objectUrl = URL.createObjectURL(file);
+      this.imagenPreview = this.sanitizer.bypassSecurityTrustUrl(objectUrl);*/
+
     }
   }
 }
