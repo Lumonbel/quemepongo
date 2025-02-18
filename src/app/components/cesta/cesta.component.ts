42a757f71c65966667dd1808daa47cacf5f48f6a
pazcasasperez <marcasper16@alu.edu.gva.es>
2025-02-18 12:27:44 +0100

    Update cesta, div, cesta llena

--- src/app/components/cesta/cesta.component.ts
+++ src/app/components/cesta/cesta.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BtAtrasComponent } from '../bt-atras/bt-atras.component';
 import { Router } from '@angular/router';
+import { CajoneraComponent } from '../cajonera/cajonera.component';
 
 @Component({
   selector: 'app-cesta',
-  imports: [BtAtrasComponent],
+  imports: [BtAtrasComponent, CajoneraComponent],
   templateUrl: './cesta.component.html',
   styleUrl: './cesta.component.css'
 })
